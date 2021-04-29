a52b41c664a709e929af52f5a76c8169f1605a89
NgxHaiiDanx <83350993+NgxHaiiDanx@users.noreply.github.com>
2021-04-29 19:28:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,29 @@
-import re
-import csv
-import numpy as np
 import pandas as pd
+# !pip install vncorenlp
 data = pd.read_excel('data.xlsx',index_col=None,na_values=['NA'],usecols="B")
-print('raw_data \n',data.head(1))
+print('raw_data \n',data.head(5))
 
 # Removing URL in data
 data = data['content'] = data['content'].map(lambda x: x.replace('https://',' ').replace('http://',' '))
 print('done_remove_url')
+print(data.head(5))
 
 # Removing new line in data
-
 data = data['content'] = data['content'].map(lambda x: x.replace('\n',' '))
 print('done_remove_newline')
-print(data.head(1))
+print(data.head(5))
 
 # Removing punctuations
-# punc = '\','/','`','*','_','{','}','[',']','(',')','>','#','+','!','$'
-
-for i in data['content']:
-    data['content'] = data
-
+# punc = ['\','/',"`",'*',"_","{","}","[","]","(",")",">","+","!","$"]
+punc = """[\/`?*_={}[]()<>+!%$^@]"""
+for i in punc:
+    print(i)
+for j in data['content']:
+    for i in j:
+        data = j.replace(i,'')
 print(data.head(10))
 
-# ##segment word
-# segment = word_tokenize(data)
-# print('done_segment')
-#
-# # Removing punctuations
-# punc= '''!()[]{};:'"\,<>./?@#$%^&*_~'''
-# def remove_punc(x):
-#     new_list=[]
-#     for i in x:
-#         if i not in punc:
-#             new_list.append(i)
-#     return new_list
-# segment=remove_punc(segment)
-# print('done_remove_punc')
-#
-# # lower case
-# segment = [word.lower() for word in segment]
-# print('done_lower_case')
-#
-# # Removing stopwords
-# with open ('vietnamese-stopwords.txt') as stop:
-#     stop_word=" ".join(line.strip("\n") for line in stop)
-#     def remove_stopwords(segment1):
-#         new_words =[]
-#         for word in segment1:
-#             if word not in stop_word:
-#                 new_words.append(word)
-#         return new_words
-#     segment_final=remove_stopwords(segment)
-# segment = None
-# print('done_remove_stopword')
-#
-# # Frequency
-# freq=Counter(segment_final)
-# most_occour=freq.most_common(50)
-# print(most_occour)
+# removing stop words
+with open('vietnamese-stopword.txt') as f:
+    sw=("".join(line.strip("\n") for line in f))
+print(sw)
